41388ea8726c74caaa1ebd9e7a4b7746eb01e3e3
Joe <josephedwardwork@gmail.com>
2020-01-01 18:22:30 -0500

    commit

--- src/java/GreetingController.java
+++ src/java/GreetingController.java
@@ -1,4 +1,4 @@
-package m1;
+package java.m1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/java/Java_React_Test_1.java
+++ src/java/Java_React_Test_1.java
@@ -1,5 +1,5 @@
 
-package m1;
+package java.m1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
